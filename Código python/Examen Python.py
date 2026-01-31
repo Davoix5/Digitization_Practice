c3197b84d6adc1403a529168853fb072766228b9
Davoix5 <davidcorralesgarrido@gmail.com>
2026-01-31 21:36:18 +0100

    Se ha pegado el código de Python

---
+++ Código python/Examen Python.py
@@ -0,0 +1,54 @@
+while True:
+    print("1 Variables")
+    print("2 Diccionarios")
+    print("3 Juego")
+    
+    opcion = input("Elije la opción que desee realiza:")
+    
+    if opcion =="1":
+        num1=int(input("Introduzca el primer número:"))
+        num2=int(input("Introduzca el segundo número:"))
+        
+        print("Hay una diferencia de ",num1-num2 )
+        if num1-num2 <10:
+            print("Números cercanos")
+        else:
+            print("Números cercanos")
+        
+        
+    elif opcion == "2":
+        animales = {"perro": "guau", "gato": "miau", "vaca": "muuu", "oveja": "beee"}
+        nombre=input("Introduce un nombre de animal o cree uno nuevo: ")
+        
+        if nombre in animales:
+            print(animales.get(nombre))
+        
+        elif nombre not in animales:
+            newanimal=input("introduzca el nuevo animal: ")
+            animales[nombre]=newanimal
+            newsound=input ("introduzca el nuevo sonido: ")
+            animales[nombre]=newsound
+            print("Nuevo animal y sonido añadido ")
+      
+        
+    elif opcion == "3":
+        import random
+        numero = random.randint(1, 51)
+        intentos=0
+        while intentos <5:
+            intento = int(input("Dime un número entre el 1 y 50: "))
+            intentos=intentos +1
+            if intento < numero:
+                print("Demasiado bajo")
+            elif intento > numero:
+                print("Demasiado alto")
+
+            elif (numero==intento):
+                print("¡Correcto!")
+                intentos = 5
+        else:
+            print("Fin del juego. El número era" )
+            print (numero)
+            
+    else:
+        print("Opción no válida")
